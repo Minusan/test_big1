29da74a7864d20d22e1121d91fc22d5f2cf244b8
Cosmin Sipos <cosmin.sipos@sustainalytics.com>
2020-02-19 11:40:00 -0500

    Added PS and ports

--- canvas-test-01/install_nginx.ps1
+++ canvas-test-01/install_nginx.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-New-Item -ItemType Directory -Force -Path F:\nginx
+New-Item -ItemType Directory -Force -Path C:\nginx
 
-$origin = "F:\nginx"
+$origin = "C:\nginx"
 $url = "http://nginx.org/download/nginx-1.16.1.zip"
 $output = "$origin\nginx-1.16.1.zip"
 $path = "$origin\nginx-1.16.1"
@@ -11,4 +11,5 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Path $output -DestinationPath $origin -Force 
 Set-Location $path
 start .\nginx.exe
-
+New-NetFirewallRule -Name HTTP -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 80
+New-NetFirewallRule -Name HTTPS -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 443
