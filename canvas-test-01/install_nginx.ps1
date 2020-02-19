b78ca655fc4b8c2f1b914f52c0a7529818297fa3
Cosmin Sipos <cosmin.sipos@sustainalytics.com>
2020-02-19 13:33:13 -0500

    Added more to the script

--- canvas-test-01/install_nginx.ps1
+++ canvas-test-01/install_nginx.ps1
@@ -11,6 +11,27 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Path $output -DestinationPath $origin -Force 
 Set-Location $path
 start .\nginx.exe
+#Create Firewall rule
 New-NetFirewallRule -Name HTTP -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 80
 New-NetFirewallRule -Name HTTPS -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 443
+#Initialize Partition
+Get-Disk -FriendlyName "Msft Virtual Disk" | Initialize-Disk -PartitionStyle MBR
+New-Partition -DiskNumber 2 -DriveLetter G -UseMaximumSize
+Format-Volume -DriveLetter G -FileSystem NTFS
+#Prepare for anisble
+New-Item -Path "c:\" -Name "temp" -ItemType "directory"
+Set-Location -Path C:\temp
+$hostname = $env:COMPUTERNAME
+$cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $hostname | Select-Object -Property Thumbprint
+$cert = Get-ChildItem -path cert:\LocalMachine\My -DnsName $env:COMPUTERNAME | Select-Object -Property Thumbprint
+$thumb = $cert.thumbprint
+$2ndhostname = """$hostname"""
+$2thumb = """$thumb"""
+$1ststring = 'winrm create winrm/config/listener?Address=*+Transport=HTTPS @{Hostname='
+$2ststring = ';CertificateThumbprint='
+$3rdstring = ';Port="5986"}'
+Out-File -FilePath C:\temp\addcertificate.bat -NoNewline -InputObject $1ststring,$2ndhostname,$2ststring,$2thumb,$3rdstring -Encoding ASCII
+cmd /C C:\temp\addcertificate.bat
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile c:\temp\ConfigureRemotingForAnsible.ps1
+& "c:\temp\ConfigureRemotingForAnsible.ps1"
 exit
