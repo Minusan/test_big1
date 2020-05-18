a6167faeb91dd88de1925c7b98a467b614725cb7
Cosmin Sipos <cosmin.sipos@sustainalytics.com>
2020-05-18 11:37:40 +0300

    update deployscript

--- canvas-sustain2/install_nginx.ps1
+++ canvas-sustain2/install_nginx.ps1
@@ -13,11 +13,17 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Path $output -DestinationPath $origin -Force 
 Set-Location $path
 start .\nginx.exe
+#create Nginx as a service
+New-Item -ItemType Directory -Force -Path "C:\nssm\"
+Invoke-WebRequest -Uri "http://nssm.cc/release/nssm-2.24.zip" -OutFile "C:\nssm\nssm-2.24.zip"
+Expand-Archive -Path "C:\nssm\nssm-2.24.zip" -DestinationPath 'C:\nssm'
+Start-Process -FilePath C:\nssm\nssm-2.24\win64\nssm.exe -ArgumentList "install nginx F:\nginx\nginx-1.16.1\nginx.exe"
 #Create Firewall rule
 New-NetFirewallRule -Name HTTP -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 80
 New-NetFirewallRule -Name HTTPS -DisplayName HTTP -Action Allow -Protocol TCP -LocalPort 443
 New-NetFirewallRule -Name nginxTCP -DisplayName nginxTCP -Action Allow -Profile Any -Protocol TCP -Program "F:\nginx\nginx-1.16.1\nginx.exe"
 New-NetFirewallRule -Name nginxUDP -DisplayName nginxUDP -Action Allow -Profile Any -Protocol TCP -Program "F:\nginx\nginx-1.16.1\nginx.exe"
+New-NetFirewallRule -Name FlashApp -DisplayName FlaskApp -Action Allow -Protocol TCP -LocalPort 10280
 
 #Prepare for anisble
 New-Item -Path "c:\" -Name "temp" -ItemType "directory"
@@ -38,4 +44,14 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/e
 #preparte for PRTG
 Install-WindowsFeature -Name SNMP-Service
 Install-WindowsFeature -Name RSAT-SNMP
+Install-WindowsFeature -name Web-Server,Web-CGI -IncludeManagementTools
+
+#install Chrome
+$Path = $env:TEMP
+$Installer = "chrome_installer.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Remove-Item $Path\$Installer -Force
+#set account password not to expire
+Set-LocalUser -Name sustadmin -PasswordNeverExpires 1
 exit
