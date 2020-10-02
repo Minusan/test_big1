1bccffe650e214322f7b1bcf683fb0920fd57a71
Cosmin Sipos <cosmin.sipos@gmail.com>
2020-10-01 22:58:43 -0400

    changed user data

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -30,13 +30,13 @@ resource "aws_instance" "instance" {
   key_name = var.key_name
 
   #start-up script
-  user_data = << EOF
-              $Path = $env:TEMP
+  user_data = "$Path = $env:TEMP
               $Installer = "chrome_installer.exe"
               Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
               Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
-              Remove-Item $Path\$Installer -Force
-  EOF
+              Remove-Item $Path\$Installer -Force "
+              
+
 
 #  provisioner "remote-exec" {
 #      inline = [  
